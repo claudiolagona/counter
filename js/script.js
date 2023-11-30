e6db45c8c432007442fff139a3bab8d11fbfbf98
Claudio La Gona <79311818+claudiolagona@users.noreply.github.com>
2023-11-30 22:20:11 +0100

    v4

--- js/script.js
+++ js/script.js
@@ -1,32 +1,45 @@
+//Creation of the rows
 const rowFirst = document.createElement("div");
-const rowSecond = document.createElement("div");
-const rowThird = document.createElement("div");
-const rowFourth = document.createElement("div");
-const heading = document.createElement("h1");
-const span = document.createElement("span");
-const increaseButton = document.createElement("button");
-const decreaseButton = document.createElement("button");
-const resetButton = document.createElement("button");
 rowFirst.setAttribute("class", "row");
 rowFirst.id = "row-first";
-heading.textContent = "Your Count is:";
+
+const rowSecond = document.createElement("div");
 rowSecond.setAttribute("class", "row");
 rowSecond.id = "row-second";
-span.textContent = "0";
-span.id = "result";
+
+const rowThird = document.createElement("div");
 rowThird.setAttribute("class", "row");
 rowThird.id = "row-third";
-increaseButton.id = "increase";
-increaseButton.textContent = "+";
-decreaseButton.id = "decrease";
-decreaseButton.textContent = "-";
+
+const rowFourth = document.createElement("div");
 rowFourth.setAttribute("class", "row");
 rowFourth.id = "row-fourth";
+
+//Creation of text elements
+const heading = document.createElement("h1");
+heading.textContent = "Your Count is:";
+
+const span = document.createElement("span");
+span.textContent = "0";
+span.id = "result";
+
+//Creation of the buttons
+const increaseButton = document.createElement("button");
+increaseButton.id = "increase";
+increaseButton.textContent = "+";
+increaseButton.setAttribute("class", "interaction active");
+
+const decreaseButton = document.createElement("button");
+decreaseButton.id = "decrease";
+decreaseButton.textContent = "-";
+decreaseButton.setAttribute("class", "interaction active");
+
+const resetButton = document.createElement("button");
 resetButton.id = "reset";
 resetButton.textContent = "Reset";
-increaseButton.setAttribute("class", "interaction active");
-decreaseButton.setAttribute("class", "interaction active");
 resetButton.setAttribute("class", "interaction active");
+
+//Append elements
 document.getElementById("container").append(rowFirst);
 document.getElementById("row-first").append(heading);
 document.getElementById("container").append(rowSecond);
@@ -37,6 +50,7 @@ document.getElementById("row-third").append(increaseButton);
 document.getElementById("container").append(rowFourth);
 document.getElementById("row-fourth").append(resetButton);
 
+//Initialize counter
 let currentResult = span;
 let result = parseInt(span.innerHTML);
 
